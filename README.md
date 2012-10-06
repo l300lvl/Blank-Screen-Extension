c4b8ddb489b4cbf7c0d767de86382dc53333132a
l300lvl <atz3@yahoo.ca>
2012-10-06 02:12:41 -0500

    fixed for 3.6 and 3.5.4

---
+++ README.md
@@ -0,0 +1,19 @@
+### Description: 
+
+Adds a Blank Screen option, to turn the screen off, to the User Menu(aka status menu). Also works from, and doesn't close, the Activites Overview. Useful if you have a long lock time set and/or just want to turn the screen off without locking it. Credit to xtranophilist.
+
+Much credit to Obsidien for his help and code skills, I suggest you check out his amazing work:
+
+https://github.com/Obsidien
+
+
+### 1 click install from E.G.O:
+
+https://extensions.gnome.org/extension/242/blank-screen/
+
+The included zip can be used to install via gnome-tweak-tool. After installing with gtt a shell restart may be required, Alt+F2, r, Enter.
+
+
+### Screenshot
+
+![Screenshot](https://raw.github.com/l300lvl/Blank-Screen-Extension/master/screenshot.png)
--- blank-screen@l300lvl.co.nr/extension.js
+++ blank-screen@l300lvl.co.nr/extension.js
@@ -1,5 +1,6 @@
 
 const St = imports.gi.St;
+const Config = imports.misc.config;
 const Main = imports.ui.main;
 const Lang = imports.lang;
 const Shell = imports.gi.Shell;
@@ -14,19 +15,37 @@ function _onBlankScreen() {
     app.activate();
 }
 
-function init() {
-    menu = Main.panel._statusArea.userMenu.menu;
-}
-
 function enable() {
     item = new PopupMenu.PopupMenuItem(_("Blank Screen"));
     item.connect('activate', Lang.bind(item, _onBlankScreen));
     let nItems = menu.numMenuItems;
-    menu.addMenuItem(item, nItems - 1);
+    menu.addMenuItem(item, nItems - 2);
 }
 
 function disable() {
     if (item) {
         item.destroy();
     }
+}
+
+let age;
+
+function init() {
+    let current_version = Config.PACKAGE_VERSION.split('.')
+    if (current_version.length != 3 || current_version[0] != 3) throw new Error("Strange version number (extension.js:22).")
+    
+    switch (current_version[1]) {
+        case"2": global.log("Warning of extension [" + metadata.uuid + "]:\n              Old development release detected (" + Config.PACKAGE_VERSION + "). You should upgrade!\n")   //eak
+        case"3":
+        case"4": age = "old"
+            break
+        case"5": global.log("Warning of extension [" + metadata.uuid + "]:\n              Development release detected (" + Config.PACKAGE_VERSION + "). Loading as a 3.6 release.\n") //eak
+        case"6": age = "new"
+            break
+        default: throw new Error("Strange version number (extension.js:32).")
+    }
+
+    if (age=="old") menu = Main.panel._statusArea.userMenu.menu
+    else            menu = Main.panel.statusArea.userMenu.menu
+
 }
