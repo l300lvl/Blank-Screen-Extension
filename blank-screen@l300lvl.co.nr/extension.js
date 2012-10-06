087e6bc75c1d1551a9df281976123584d980ce4f
l300lvl <atz3@yahoo.ca>
2012-10-06 02:20:15 -0500

    fixed for 3.6 and 3.5.4

--- blank-screen@l300lvl.co.nr/extension.js
+++ blank-screen@l300lvl.co.nr/extension.js
@@ -32,7 +32,7 @@ let age;
 
 function init() {
     let current_version = Config.PACKAGE_VERSION.split('.')
-    if (current_version.length != 3 || current_version[0] != 3) throw new Error("Strange version number (extension.js:22).")
+    if (current_version.length != 3 || current_version[0] != 3) throw new Error("Strange version number (extension.js:35).")
     
     switch (current_version[1]) {
         case"2": global.log("Warning of extension [" + metadata.uuid + "]:\n              Old development release detected (" + Config.PACKAGE_VERSION + "). You should upgrade!\n")   //eak
@@ -42,7 +42,7 @@ function init() {
         case"5": global.log("Warning of extension [" + metadata.uuid + "]:\n              Development release detected (" + Config.PACKAGE_VERSION + "). Loading as a 3.6 release.\n") //eak
         case"6": age = "new"
             break
-        default: throw new Error("Strange version number (extension.js:32).")
+        default: throw new Error("Strange version number (extension.js:45).")
     }
 
     if (age=="old") menu = Main.panel._statusArea.userMenu.menu
