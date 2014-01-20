616c5be3a5eb7f1b3f417c8d20aed2b8904f861f
Reuben Thomas <rrt@sc3d.org>
2014-01-20 01:48:15 +0000

    Update for GNOME 3.10; remove backwards compatiblity

--- blank-screen@l300lvl.co.nr/extension.js
+++ blank-screen@l300lvl.co.nr/extension.js
@@ -19,7 +19,7 @@ function enable() {
     item = new PopupMenu.PopupMenuItem(_("Blank Screen"));
     item.connect('activate', Lang.bind(item, _onBlankScreen));
     let nItems = menu.numMenuItems;
-    menu.addMenuItem(item, nItems - 2);
+    menu.addMenuItem(item, nItems);
 }
 
 function disable() {
@@ -28,24 +28,6 @@ function disable() {
     }
 }
 
-let age;
-
 function init() {
-    let current_version = Config.PACKAGE_VERSION.split('.')
-    if (current_version.length != 3 || current_version[0] != 3) throw new Error("Strange version number (extension.js:35).")
-    
-    switch (current_version[1]) {
-        case"2": global.log("Warning of extension [" + metadata.uuid + "]:\n              Old development release detected (" + Config.PACKAGE_VERSION + "). You should upgrade!\n")   //eak
-        case"3":
-        case"4": age = "old"
-            break
-        case"5": global.log("Warning of extension [" + metadata.uuid + "]:\n              Development release detected (" + Config.PACKAGE_VERSION + "). Loading as a 3.6 release.\n") //eak
-        case"6": age = "new"
-            break
-        default: throw new Error("Strange version number (extension.js:45).")
-    }
-
-    if (age=="old") menu = Main.panel._statusArea.userMenu.menu
-    else            menu = Main.panel.statusArea.userMenu.menu
-
+     menu = Main.panel.statusArea.aggregateMenu.menu
 }
